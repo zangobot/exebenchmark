a7776fa4a9cdc3a92b0b5ba02a7c99fd4c280f41
danielgibert <danigibert24@gmail.com>
2025-01-24 11:52:37 +0100

    Minor bug with configuration file

--- scripts/training/EMBER/train_MalConv.sh
+++ scripts/training/EMBER/train_MalConv.sh
@@ -1,3 +1,3 @@
 cd ../../../
 
-python train_end2end_detector.py configurations/malconv_ember_configuration_file.json
+python train_end2end_detector.py configurations/malconv_ember_configuration_file_1MB.json
