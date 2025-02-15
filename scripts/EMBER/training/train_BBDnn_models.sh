aa4f1b1390c24da8054b3ee5b7eecdda6c93810e
danielgibert <danigibert24@gmail.com>
2025-02-15 22:52:26 +0100

    Added scripts for training AvastConv, BBDnn, NGramConv, and ResNet18

---
+++ scripts/EMBER/training/train_BBDnn_models.sh
@@ -0,0 +1,10 @@
+cd ../../../
+
+python train_end2end_detector.py  configurations/EMBER/training/BBDnn/bbdnn_ember_configuration_file.json
+python train_end2end_detector.py  configurations/EMBER/training/BBDnn/rs_bbdnn_ember_configuration_file.json
+python train_end2end_detector.py  configurations/EMBER/training/BBDnn/rsdel_bbdnn_ember_configuration_file.json
+python train_end2end_detector.py  configurations/EMBER/training/BBDnn/drs_bbdnn_ember_configuration_file.json
+python train_end2end_detector.py  configurations/EMBER/training/BBDnn/sequential_drs_bbdnn_ember_configuration_file.json
+python train_end2end_detector.py  configurations/EMBER/training/BBDnn/random_drs_bbdnn_ember_configuration_file.json
+
+
---
+++ scripts/EMBER/training/train_ResNet18_models.sh
@@ -0,0 +1,3 @@
+cd ../../../
+
+python train_end2end_detector.py  configurations/EMBER/training/ResNet18/resnet18_ember_configuration_file.json
