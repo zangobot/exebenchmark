b2e89eb2fd72b6fb9595938e6d5bc70d874394db
danielgibert <danigibert24@gmail.com>
2025-02-27 10:10:56 +0100

    Looking for the bug

--- scripts/EMBER/train_validate_and_test_NN_models.sh
+++ scripts/EMBER/train_validate_and_test_NN_models.sh
@@ -1,10 +1,12 @@
 cd ../../
 
 # ResNet18
-python train_end2end_detector.py  configurations/EMBER/training/ResNet18/resnet18_ember_configuration_file.json
+#python train_end2end_detector.py  configurations/EMBER/training/ResNet18/resnet18_ember_configuration_file.json
 python evaluate_end2end_detector.py  configurations/EMBER/validation/ResNet18/resnet18_ember_validation_set_configuration_file.json
 python evaluate_end2end_detector.py  configurations/EMBER/testing/ResNet18/resnet18_ember_test_set_configuration_file.json
 
+
+!'
 # AvastConv
 python train_end2end_detector.py  configurations/EMBER/training/AvastConv/avastconv_ember_configuration_file.json
 python train_end2end_detector.py  configurations/EMBER/training/AvastConv/rs_avastconv_ember_configuration_file.json
@@ -93,3 +95,4 @@ python evaluate_end2end_detector.py  configurations/EMBER/testing/MalConv/rsdel_
 python evaluate_end2end_detector.py  configurations/EMBER/testing/MalConv/drs_malconv_ember_test_set_configuration_file.json
 python evaluate_end2end_detector.py  configurations/EMBER/testing/MalConv/sequential_drs_malconv_ember_test_set_configuration_file.json
 python evaluate_end2end_detector.py  configurations/EMBER/testing/MalConv/random_drs_malconv_ember_test_set_configuration_file.json
+'
