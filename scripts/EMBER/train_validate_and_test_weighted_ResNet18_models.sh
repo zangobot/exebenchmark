0b5d5c13642a1e357ab3901b45a03906d6316328
danielgibert <danigibert24@gmail.com>
2025-04-03 20:47:59 +0200

    Fixed path to test configuration ResNet18

--- scripts/EMBER/train_validate_and_test_weighted_ResNet18_models.sh
+++ scripts/EMBER/train_validate_and_test_weighted_ResNet18_models.sh
@@ -2,4 +2,4 @@ cd ../../
 
 #python train_end2end_detector.py  configurations/EMBER/training/ResNet18/resnet18_ember_configuration_file_pos_weight_0.875.json
 #python evaluate_end2end_detector.py  configurations/EMBER/validation/ResNet18/resnet18_ember_validation_set_configuration_file_pos_weight_0.875.json
-python evaluate_end2end_detector.py  configurations/EMBER/test/ResNet18/resnet18_ember_test_set_configuration_file_pos_weight_0.875.json
+python evaluate_end2end_detector.py  configurations/EMBER/testing/ResNet18/resnet18_ember_test_set_configuration_file_pos_weight_0.875.json
