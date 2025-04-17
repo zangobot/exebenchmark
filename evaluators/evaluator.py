99f6876a8037e9a20597e30f88363304ddf30a77
zangobot <luca.demetrio@outlook.it>
2025-04-17 13:37:25 +0200

    Refactoring of the evaluator part 2

--- evaluators/evaluator.py
+++ evaluators/evaluator.py
@@ -1,20 +1,24 @@
+import abc
+
 from maltorch.data_processing.grayscale_preprocessing import GrayscalePreprocessing
 from maltorch.data_processing.majority_voting_postprocessing import MajorityVotingPostprocessing
 from maltorch.data_processing.rs_preprocessing import RandomizedAblationPreprocessing
 from maltorch.data_processing.rsdel_preprocessing import RandomizedDeletionPreprocessing
+from maltorch.data_processing.sigmoid_postprocessor import SigmoidPostprocessor
 from maltorch.zoo.avaststyleconv import AvastStyleConv
 from maltorch.zoo.bbdnn import BBDnn
 from maltorch.zoo.ember_gbdt import EmberGBDT
 from maltorch.zoo.malconv import MalConv
 from maltorch.zoo.ngramconv import NGramConv
 from maltorch.zoo.resnet18 import ResNet18
+from torch.utils.data import DataLoader
 
 import utils
 from config import ZOO_PATH
 from utils import read_json_file
 
 
-class Evaluator:
+class Evaluator(abc.ABC):
     def __init__(self, config_path):
         self.config = read_json_file(config_path)
         self.model = self.build_model(self.config)
@@ -25,13 +29,15 @@ class Evaluator:
         device = utils.check_cuda()
 
         ablation_preprocessing = RandomizedAblationPreprocessing(pabl=0.20, num_versions=100, padding_idx=256)
-        voting_postprocessing = MajorityVotingPostprocessing()
+        voting_postprocessing = MajorityVotingPostprocessing(apply_sigmoid=True)
+        sigmoid_postprocessor = SigmoidPostprocessor()
 
         if architecture_name == "Malconv":
             return (
                 MalConv().create_model(
                     model_path=ZOO_PATH / architecture_name,
-                    device=device
+                    device=device,
+                    postprocessing=sigmoid_postprocessor
                 )
             )
         if architecture_name == "EmberGBDT":
@@ -43,19 +49,22 @@ class Evaluator:
         if architecture_name == "AvastStyleConv":
             return (
                 AvastStyleConv().create_model(
-                    model_path=ZOO_PATH / architecture_name, device=device
+                    model_path=ZOO_PATH / architecture_name, device=device,
+                    postprocessing=sigmoid_postprocessor
                 )
             )
         if architecture_name == "BBDnn":
             return (
                 BBDnn().create_model(
-                    model_path=ZOO_PATH / architecture_name, device=device
+                    model_path=ZOO_PATH / architecture_name, device=device,
+                    postprocessing=sigmoid_postprocessor
                 )
             )
         if architecture_name == "NGramConv":
             return (
                 NGramConv().create_model(
-                    model_path=ZOO_PATH / architecture_name, device=device
+                    model_path=ZOO_PATH / architecture_name, device=device,
+                    postprocessing=sigmoid_postprocessor
                 )
             )
         if architecture_name == "ResNet18":
@@ -162,5 +171,10 @@ class Evaluator:
         if architecture_name == "MalConvDRS":
             pass
 
-    def evaluate(self, x):
-        return self
+    @abc.abstractmethod
+    def load_data(self) -> DataLoader:
+        ...
+
+    @abc.abstractmethod
+    def evaluate(self, data_loader: DataLoader) -> DataLoader:
+        ...
