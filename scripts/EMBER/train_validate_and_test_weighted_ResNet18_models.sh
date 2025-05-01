823418dc0a6b694c44160d1b66af8bc847fc1b22
danielgibert <danigibert24@gmail.com>
2025-05-01 22:01:35 +0200

    Minor

--- scripts/EMBER/train_validate_and_test_weighted_ResNet18_models.sh
+++ scripts/EMBER/train_validate_and_test_weighted_ResNet18_models.sh
@@ -1,5 +1,5 @@
 cd ../../
 
-python train_end2end_detector.py  configurations/EMBER/training/ResNet18/resnet18_ember_configuration_file_pos_weight_0.875.json
+#python train_end2end_detector.py  configurations/EMBER/training/ResNet18/resnet18_ember_configuration_file_pos_weight_0.875.json
 python evaluate_end2end_detector.py  configurations/EMBER/validation/ResNet18/resnet18_ember_validation_set_configuration_file_pos_weight_0.875.json
 python evaluate_end2end_detector.py  configurations/EMBER/testing/ResNet18/resnet18_ember_test_set_configuration_file_pos_weight_0.875.json
