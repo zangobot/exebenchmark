e07f89f1ae99815afd5611a7d53c4de960d26420
danielgibert <danigibert24@gmail.com>
2025-07-01 10:04:22 +0200

    Prepare scripts to rerun evaluator on test and val set for RsDel models

---
+++ scripts/EMBER/evaluator/test_models_on_EMBER_validation_set.sh
@@ -0,0 +1,102 @@
+cd ../../../evaluators/
+
+!'
+# MalConv models
+python evaluator.py EMBER/validation/MalConv/ember_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_512_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_2048_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_4096_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_8192_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_16384_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_32768_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_65536_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_fdrs_131072_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_kdrs_4_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_kdrs_8_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_kdrs_12_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_kdrs_16_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_kdrs_20_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_rdrs_0.05_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_rdrs_0.10_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_rdrs_0.20_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_sdrs_0.05_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_sdrs_0.10_malconv_config.json
+python evaluator.py EMBER/validation/MalConv/ember_sdrs_0.20_malconv_config.json
+
+# NgramConv models
+python evaluator.py EMBER/validation/NGramConv/ember_ngramconv_config.json
+
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_512_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_2048_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_4096_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_8192_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_16384_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_32768_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_65536_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_fdrs_131072_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_kdrs_4_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_kdrs_8_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_kdrs_12_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_kdrs_16_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_kdrs_20_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_rdrs_0.05_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_rdrs_0.10_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_rdrs_0.20_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_sdrs_0.05_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_sdrs_0.10_ngramconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_sdrs_0.20_ngramconv_config.json
+
+# AvastConv models
+python evaluator.py EMBER/validation/AvastConv/ember_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_fdrs_16384_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_fdrs_32768_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_fdrs_65536_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_fdrs_131072_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_kdrs_4_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_kdrs_8_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_kdrs_12_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_kdrs_16_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_kdrs_20_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_rdrs_0.05_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_rdrs_0.10_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_rdrs_0.20_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_sdrs_0.05_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_sdrs_0.10_avastconv_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_sdrs_0.20_avastconv_config.json
+
+# BBDnn models
+python evaluator.py EMBER/validation/BBDnn/ember_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_fdrs_4096_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_fdrs_8192_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_fdrs_16384_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_fdrs_32768_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_fdrs_65536_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_fdrs_131072_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_kdrs_4_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_kdrs_8_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_kdrs_12_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_kdrs_16_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_kdrs_20_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_rdrs_0.05_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_rdrs_0.10_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_rdrs_0.20_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_sdrs_0.05_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_sdrs_0.10_bbdnn_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_sdrs_0.20_bbdnn_config.json
+
+# ResNet18
+python evaluator.py EMBER/validation/ResNet/ember_resnet_config.json
+
+# LightGBM
+python evaluator.py EMBER/validation/LightGBM/ember_lightgbm_config.json
+
+python evaluator.py EMBER/validation/MalConv/ember_rs_malconv_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_rs_bbdnn_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_rs_avastconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_rs_ngramconv_config.json
+'
+python evaluator.py EMBER/validation/MalConv/ember_rsdel_malconv_config.json
+python evaluator.py EMBER/validation/BBDnn/ember_rsdel_bbdnn_config.json
+python evaluator.py EMBER/validation/AvastConv/ember_rsdel_avastconv_config.json
+python evaluator.py EMBER/validation/NGramConv/ember_rsdel_ngramconv_config.json
+
